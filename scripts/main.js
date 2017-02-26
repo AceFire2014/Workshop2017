7a9bb54a1b30dae855b4082f5caf05a3a8a5a78d
AceFire <AceFire2015@gmail.com>
2017-02-27 01:14:14 +0800

    add a image changer script

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,9 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
+var myImage = document.querySelector('img');
+myImage.onclick = function(){
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/img1.jpeg'){
+        myImage.setAttribute('src', 'images/img2.jpeg');
+    } else {
+        myImage.setAttribute('src', 'images/img1.jpeg')
+    }
+}
