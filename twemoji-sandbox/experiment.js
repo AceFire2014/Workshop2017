e087ad14b91c0effbaeb5471cd04480ba414915f
Andy Tsai <andytsai007@gmail.com>
2017-02-28 12:43:10 +0800

    twemoji.parse() with a mix of emoji and regular text

--- twemoji-sandbox/experiment.js
+++ twemoji-sandbox/experiment.js
@@ -1,2 +1,18 @@
+//output
+//<img class="emoji" draggable="false" alt="😺" src="https://twemoji.maxcdn.com/2/72x72/1f63a.png">
+/*
+var twemoji = require('twemoji');
+console.log(twemoji.parse('😺'));
+*/
+
+// output
+// <img class="emoji" draggable="false" alt="😺" src="https://twemoji.maxcdn.com/2/72x72/1f63a.png"><img class="emoji" draggable="false" alt="❤️" src="https://twemoji.maxcdn.com/2/72x72/2764.png">
+/*
 var twemoji = require('twemoji');
 console.log(twemoji.parse('😺❤️'));
+*/
+
+//output
+//This is <img class="emoji" draggable="false" alt="😺" src="https://twemoji.maxcdn.com/2/72x72/1f63a.png">
+var twemoji = require('twemoji');
+console.log(twemoji.parse('This is 😺'));
